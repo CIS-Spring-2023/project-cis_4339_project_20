922008976e2027ac70966e922a8acf1673af04f3
tommy <tommyqle9@gmail.com>
2023-03-12 23:15:32 -0500

    Added input box to 'add services' page

--- frontend/src/components/addServices.vue
+++ frontend/src/components/addServices.vue
@@ -14,20 +14,23 @@ export default {
 
 
 <template>
-  <h1 class="title"> Current List of Services </h1>
-  <div v-for="item in list.services" class="list">
-      {{ item }}
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+    <h3 class="text-center"> Current List of Services </h3>
+      <div v-for="item in list.services" class="text-center">
+            {{ item }}
+      </div>
+    <hr class="solid">
+      <h3 class="text-center mt-4"> Enter a service to add it to the list</h3>
+        <form @submit.prevent="handleSubmitForm">
+          <div class="form-group text-center">
+            <input type="text" class="mb-2 form-control text-center">
+            <button class="btn btn-primary btn-block"> Add </button>
+          </div>
+        </form>
+    </div>
   </div>
 </template>
 
 <style>
-
-.title {
-  text-align: center;
-}
-
-.list {
-  text-align: center;
-}
-
 </style> 
