e55dcc577e31d43b380c440e2260160729f2ea90
tommy <tommyqle9@gmail.com>
2023-03-13 18:55:49 -0500

    Comments and css for list

--- frontend/src/components/addServices.vue
+++ frontend/src/components/addServices.vue
@@ -32,12 +32,15 @@ export default {
       >
       Add Service
       </h1>
-    <h3 class="text-center"> Current List of Services </h3>
+      <br>
+    <h3 class="text-center"> Current List of Services: </h3>
       <div v-for="item in list.activeServices" class="text-center">
-        {{ item.name }} - Active
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"> {{ item.name }} - Active </li>
+        </ul>
       </div>
     <hr class="solid">
-      <h3 class="text-center mt-4"> Enter a service to add it to the list</h3>
+      <h3 class="text-center mt-4"> Enter a service to add it to the list.</h3>
         <form @submit.prevent="addService(newService)">
           <div class="form-group text-center">
             <input type="text" class="mb-2 form-control text-center" v-model="newService">
