275d1a68243772fcdd74965d2f5294d6cff72eda
tommy <tommyqle9@gmail.com>
2023-04-27 17:40:59 -0500

    Added comments to services.js

--- frontend/src/store/services.js
+++ frontend/src/store/services.js
@@ -6,7 +6,7 @@ const apiURL = import.meta.env.VITE_ROOT_API
 export const useServiceList = defineStore({
     id: 'list',
     state: () => ({
-        services: [],
+        services: [], // Services list is initialized for pulling all services in database.
         id: 0,
     }),
 
@@ -16,8 +16,8 @@ export const useServiceList = defineStore({
             this.services = response.data
           },
         async addService(newService) {
-            const response = await axios.post(`${apiURL}/services`, {
-                name: newService,
+            const response = await axios.post(`${apiURL}/services`, { //axios.post to get make a post request to the services backend.
+                name: newService, // The new service to be added is assigned a name and the org and active field is defaulted.
                 org: '1',
                 active: true,
             })
@@ -26,7 +26,7 @@ export const useServiceList = defineStore({
         },
     },
     getters: {
-        activeServices() {
+        activeServices() { // Filters services that are active.
             return this.services.filter(service => service.active);
         },
     },
